d30f640bb9e5a9c3147b54526225457c798e57cb
Rebecca Emmanuel <rebecca.emmanuel@koi.fritz.box>
2020-04-21 10:04:51 +0200

    added slot to headline-component and accordion

--- src/components/headline-component/template.ts
+++ src/components/headline-component/template.ts
@@ -3,10 +3,13 @@ import * as styles from './styles.scss';
 
 import { HeadlineComponentProps, HeadlineComponentState } from './defines';
 
-
-export const template = ( data: HeadlineComponentProps & HeadlineComponentState ): HTMLFragment => {
-  return html`
-    <h1 class="headline">Hello <slot name="first-name">First name</slot>!🐣</h1>
-    ${createStyle(styles)}
-  `;
-}
+export const template = (
+	data: HeadlineComponentProps & HeadlineComponentState
+): HTMLFragment => {
+	return html`
+		<h1 class="headline">
+			Hello <slot name="first-name">First name</slot>!🐣
+		</h1>
+		${createStyle(styles)}
+	`;
+};
